fc5c18e2d370f5ba35281f469bbf63c42a75d9b5
Chrisyhjiang <chrisyh.jiang@gmail.com>
2025-02-22 20:00:41 -0600

    “Added the AuthController..."

--- src/main/java/com/webcart/project/security/jwt/JwtUtils.java
+++ src/main/java/com/webcart/project/security/jwt/JwtUtils.java
@@ -17,67 +17,60 @@ import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 
-
 @Component
 public class JwtUtils {
-
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-    @Value("${spring.app.jwtSecrete}")
-    private String jwtSecrete;
+    @Value("${spring.app.jwtSecret}")
+    private String jwtSecret;
 
     @Value("${spring.app.jwtExpirationMs}")
     private int jwtExpirationMs;
 
-    public String getJwtFromHeader(HttpServletRequest request){
-        String bearToken = request.getHeader("Authorization");
-        logger.debug("Authorization Header: {}", bearToken);
-        if(bearToken != null && bearToken.startsWith("Bearer ")){
-            return bearToken.substring(7);
+    public String getJwtFromHeader(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        logger.debug("Authorization Header: {}", bearerToken);
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7); // Remove Bearer prefix
         }
         return null;
     }
 
-    public String generateTokenFromUserName(UserDetails userDetails){
-        String userName = userDetails.getUsername();
+    public String generateTokenFromUsername(UserDetails userDetails) {
+        String username = userDetails.getUsername();
         return Jwts.builder()
-                .subject(userName)
+                .subject(username)
                 .issuedAt(new Date())
                 .expiration(new Date((new Date()).getTime() + jwtExpirationMs))
                 .signWith(key())
                 .compact();
     }
 
-    public String getUserNameFromJwtToken(String token){
+    public String getUserNameFromJwtToken(String token) {
         return Jwts.parser()
-                .verifyWith((SecretKey) key())
-                .build()
-                .parseSignedClaims(token)
-                .getPayload()
-                .getSubject();
+                        .verifyWith((SecretKey) key())
+                .build().parseSignedClaims(token)
+                .getPayload().getSubject();
     }
 
-    public boolean validateJwtToken (String authToken){
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public boolean validateJwtToken(String authToken) {
         try {
-                Jwts.parser()
-                        .verifyWith((SecretKey) key())
-                        .build()
-                        .parseSignedClaims(authToken);
-                return true;
-        } catch (MalformedJwtException ex) {
-            logger.error("Invalid JWT token: {}", ex.getMessage());
-        } catch (ExpiredJwtException ex) {
-            logger.error("JWT token is expried :{}", ex.getMessage());
-        } catch (UnsupportedJwtException ex) {
-            logger.error("JWT token is unsupported: {}", ex.getMessage());
-        } catch (IllegalArgumentException ex) {
-            logger.error("JWT claims string is empty: {}", ex.getMessage());
+            System.out.println("Validate");
+            Jwts.parser().verifyWith((SecretKey) key()).build().parseSignedClaims(authToken);
+            return true;
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
         }
         return false;
     }
-
-    private Key key(){
-        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecrete));
-    }
-
 }
