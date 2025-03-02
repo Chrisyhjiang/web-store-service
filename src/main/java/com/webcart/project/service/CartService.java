a6b23289112471411c40b8bf7a234d908dfbb86a
Chrisyhjiang <chrisyh.jiang@gmail.com>
2025-03-02 13:48:06 -0600

    "Added the getAllCart endpoint..."

--- src/main/java/com/webcart/project/service/CartService.java
+++ src/main/java/com/webcart/project/service/CartService.java
@@ -2,6 +2,10 @@ package com.webcart.project.service;
 
 import com.webcart.project.payload.CartDTO;
 
+import java.util.List;
+
 public interface CartService {
     CartDTO addProductToCart(Long productId, Integer quantity);
+
+    List<CartDTO> getAllCarts();
 }
