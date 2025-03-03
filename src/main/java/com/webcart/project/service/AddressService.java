8bdc92fe396224af3ea082fd78a1c2d0d4729cdf
Chrisyhjiang <chrisyh.jiang@gmail.com>
2025-03-02 22:07:33 -0600

    "added user address..."

---
+++ src/main/java/com/webcart/project/service/AddressService.java
@@ -0,0 +1,20 @@
+package com.webcart.project.service;
+
+import com.webcart.project.model.User;
+import com.webcart.project.payload.AddressDTO;
+
+import java.util.List;
+
+public interface AddressService {
+    AddressDTO createAddress(AddressDTO addressDTO, User user);
+
+    List<AddressDTO> getAddresses();
+
+    AddressDTO getAddressesById(Long addressId);
+
+    List<AddressDTO> getUserAddresses(User user);
+
+    AddressDTO updateAddress(Long addressId, AddressDTO addressDTO);
+
+    String deleteAddress(Long addressId);
+}
